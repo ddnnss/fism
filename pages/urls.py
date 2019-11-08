08c3153f21da1c65c58dd9ff745f31ccd474170f
ddnnss <ddnnss.i1@gmail.com>
2019-11-08 07:32:41 +0300

    dfgfd

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('1/', views.index1, name='index1'),
+    path('lk/', views.lk, name='lk'),
     path('search/', views.search, name='search')
 
 
--- pages/views.py
+++ pages/views.py
@@ -5,6 +5,8 @@ def index(request):
     return render(request, 'index.html', locals())
 def index1(request):
     return render(request, 'index1.html', locals())
+def lk(request):
+    return render(request, 'lk.html', locals())
 def search(request):
     allcat = Category.objects.all()
     allfil = Filter.objects.all()
